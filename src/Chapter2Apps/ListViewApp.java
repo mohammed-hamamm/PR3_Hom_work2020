b93cf860190b500dee0569c4dfbd68b24a83062c
aashgar <aashgar@IASHGAR.iugaza.edu>
2020-02-16 19:47:17 +0200

    All Done,,,

--- src/Chapter2Apps/ListViewApp.java
+++ src/Chapter2Apps/ListViewApp.java
@@ -5,7 +5,10 @@
  */
 package Chapter2Apps;
 
+import java.util.stream.Collectors;
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
@@ -19,15 +22,46 @@ import javafx.stage.Stage;
  * @author aashgar
  */
 public class ListViewApp extends Application{
-
+    private ListView<String> listViewNames;
+    private TextField textFieldName;
+    private  Button buttonAdd;
     @Override
     public void start(Stage primaryStage) throws Exception {
-        ListView<String> listViewNames = new ListView<>();
-        TextField textFieldName = new TextField();
-        Button buttonAdd = new Button("Add New");
+        listViewNames = new ListView<>();
+        textFieldName = new TextField();
+        buttonAdd = new Button("Add New");
         VBox vBox = new VBox(10, listViewNames,textFieldName,buttonAdd);
         vBox.setAlignment(Pos.CENTER);
-        Scene scene = new Scene(vBox,200,300);
+        Scene scene = new Scene(vBox,260,300);
+        EventHandler1 eventHandler1 = new EventHandler1();
+        buttonAdd.setOnAction(eventHandler1);
+//        buttonAdd.setOnAction(new EventHandler<ActionEvent>() { //Using Anonymous class
+//            @Override
+//            public void handle(ActionEvent event) {
+//                if(event.getSource() == buttonAdd) //Checking the event source
+//                if(!textFieldName.getText().equals(""))
+//                    listViewNames.getItems().add(textFieldName.getText());
+//                textFieldName.setText("");
+//                listViewNames.getItems().setAll(
+//                        listViewNames.getItems().stream()
+//                        .sorted()
+//                        .collect(Collectors.toList())
+//                );
+//            }
+//        });
+
+        buttonAdd.setOnAction(event->{ //Using Lambda
+            if(event.getSource() == buttonAdd) //Checking the event source
+                if(!textFieldName.getText().equals(""))
+                    listViewNames.getItems().add(textFieldName.getText());
+                textFieldName.setText("");
+                listViewNames.getItems().setAll(
+                        listViewNames.getItems().stream()
+                        .sorted()
+                        .collect(Collectors.toList())
+                );
+        });
+        
         primaryStage.setTitle("ListView JavaFX App");
         primaryStage.setScene(scene);
         primaryStage.setResizable(false);
@@ -37,4 +71,19 @@ public class ListViewApp extends Application{
     public static void main(String[] args) {
         launch(args);
     }
+    private class EventHandler1 implements EventHandler<ActionEvent>{
+
+        @Override
+        public void handle(ActionEvent event) { //implement event handling using ActionEvent calss 
+            if(event.getSource() == buttonAdd) //Checking the event source
+                if(!textFieldName.getText().equals(""))
+                    listViewNames.getItems().add(textFieldName.getText());
+                textFieldName.setText("");
+                listViewNames.getItems().setAll(
+                        listViewNames.getItems().stream()
+                        .sorted()
+                        .collect(Collectors.toList())
+                );
+        }    
+    }    
 }
